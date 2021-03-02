faca728f8612cea09b97e54561571ebd8c8ec951
Luis Figueroa <luis_figueroa_morales@yahoo.com>
2021-03-02 13:51:16 -0500

    using #DEFINE

---
+++ fahrenheit-celsius-v4.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+#define LOWER	0	// lower limit
+#define UPPER	300	// upper limit
+#define STEP	20	// size of the increment
+
+// print fahrenheit - celsius table
+main(){
+    int fahr;
+
+    for (fahr = LOWER; fahr <= UPPER; fahr = fahr + STEP)
+	printf("%3d\t%6.1f\n", fahr, (5.0/9.0)*(fahr-32));
+}
